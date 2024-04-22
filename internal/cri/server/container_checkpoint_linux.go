c19dffcdffa8d5a4e97225561ce8e16d0b9e2b49
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2024-04-22 18:29:49 +0200

    fixup! Support multiplatform in image pull

--- internal/cri/server/container_checkpoint_linux.go
+++ internal/cri/server/container_checkpoint_linux.go
@@ -93,7 +93,7 @@ func (c *criService) CheckpointContainer(ctx context.Context, r *runtime.Checkpo
 	}
 
 	imageRef := container.ImageRef
-	image, err := c.GetImage(imageRef)
+	image, err := c.GetImage(imageRef, container.Config.GetImage().RuntimeHandler)
 	if err != nil {
 		return nil, fmt.Errorf("getting container image failed: %w", err)
 	}
